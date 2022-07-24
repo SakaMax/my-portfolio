8c002335cec524484d3cdd47a69f8124704b5006
SakaMax <sakaguchi.kosuke.83z@st.kyoto-u.ac.jp>
2022-07-24 13:46:03 +0900

    Hobbiesをとりあえず動く形に

---
+++ main.js
@@ -0,0 +1,72 @@
+// 趣味のコンテナ
+const hobbies = document.getElementById("hobbies")
+
+// 趣味の配列
+const hobbiesList = [
+  { title: "原付", main: "原付について", img: "./test.png" },
+  { title: "factorio", main: "factorioについて", img: "./test.png" },
+  { title: "昼寝", main: "昼寝について", img: "./test.png" },
+]
+
+/**
+ * 趣味の中身を作る
+ * @param {Number} index
+ */
+const createHobbyDiv = function (index) {
+  console.log(index)
+  const newHobbyContainer = document.createElement("div")
+  const title = document.createElement("div")
+  const main = document.createElement("div")
+  const img = document.createElement("img")
+
+  title.textContent = hobbiesList[index].title
+  main.textContent = hobbiesList[index].main
+  img.src = hobbiesList[index].img
+
+  title.classList.add("hobby-title")
+  main.classList.add("hobby-main")
+  img.classList.add("hobby-img")
+
+  newHobbyContainer.append(title)
+  newHobbyContainer.append(main)
+  newHobbyContainer.append(img)
+
+  newHobbyContainer.id = "hobby-container"
+
+  return newHobbyContainer
+}
+
+const removeHobby = function () {
+  const currentHobby = document.getElementById("hobby-container")
+  currentHobby.remove()
+}
+
+const insertHobby = function (hobbyDiv) {
+  const right = document.getElementById("hobbies-right")
+  right.before(hobbyDiv)
+}
+
+const hobbiesLeft = document.getElementById("hobbies-left")
+const hobbiesRight = document.getElementById("hobbies-right")
+let hobbiesIndex = 0
+
+hobbiesLeft.onclick = function () {
+  console.log(`Old hobbiesindex = ${hobbiesIndex}`)
+  removeHobby()
+  hobbiesIndex = (hobbiesIndex + hobbiesList.length - 1) % hobbiesList.length
+  console.log(`New hobbiesindex = ${hobbiesIndex}`)
+  const newHobby = createHobbyDiv(hobbiesIndex)
+  insertHobby(newHobby)
+}
+
+hobbiesRight.onclick = function () {
+  console.log(`Old hobbiesindex = ${hobbiesIndex}`)
+  removeHobby()
+  hobbiesIndex = (hobbiesIndex + 1) % hobbiesList.length
+  console.log(`New hobbiesindex = ${hobbiesIndex}`)
+  const newHobby = createHobbyDiv(hobbiesIndex)
+  insertHobby(newHobby)
+}
+
+const newHobby = createHobbyDiv(0)
+insertHobby(newHobby)
